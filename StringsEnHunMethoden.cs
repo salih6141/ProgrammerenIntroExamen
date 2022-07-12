58765ca064c4c67d64c5e01f217d42067e6db112
Salih Komut <salih.komut@student.ap.be>
2022-07-12 13:23:41 +0200

    bereken-btw oefening afgewerkt

--- StringsEnHunMethoden.cs
+++ StringsEnHunMethoden.cs
@@ -13,5 +13,17 @@ namespace ProgrammerenIntroExamen
             string invoer = Console.ReadLine();
             Console.WriteLine(invoer.ToUpper());
         }
+
+        public static void BerekenBtw()
+        {
+            Console.Write("Geef het bedrag in: ");
+            int bedrag = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Geef BTW percentage in: ");
+            int btwPercentage = Convert.ToInt32(Console.ReadLine());
+            int bedragInclusiefBtw = bedrag + (bedrag/100*btwPercentage);
+            Console.WriteLine($"Het bedrag {bedrag} met {btwPercentage}% btw bedraagt {bedragInclusiefBtw}");
+        }
+
+
     }
 }
