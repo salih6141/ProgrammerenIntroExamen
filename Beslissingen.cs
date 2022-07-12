40e02ea173eccafdb4497b420dc2876ed6d94923
Salih Komut <salih.komut@student.ap.be>
2022-07-12 14:25:49 +0200

    basis van de oefening uitgewerkt en getest

--- Beslissingen.cs
+++ Beslissingen.cs
@@ -9,7 +9,20 @@ namespace ProgrammerenIntroExamen
     {
         public static void Schoenenverkoper()
         {
-            
+            double prijsSchoen = 50.00;
+            double bedrag = 00.00;
+            Console.WriteLine("Hoeveel paar schoenen wil je kopen?");
+            int aantalSchoenen = Convert.ToInt32(Console.ReadLine());
+            if (aantalSchoenen > 1)
+            {
+                bedrag = Math.Round((aantalSchoenen * prijsSchoen) * 0.9 , 2);
+                Console.WriteLine($"De prijs voor {aantalSchoenen} paar schoenen is {bedrag}");
+            }
+            else
+            {
+                bedrag = Math.Round(aantalSchoenen * prijsSchoen,2);
+                Console.WriteLine($"De prijs voor {aantalSchoenen} paar schoenen is {bedrag}");
+            }
         }
     }
 }
